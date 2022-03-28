da6459e239c294276aafdf905572ebb90e2283aa
NarawitC <narawit.developer@outlook.com>
2022-03-28 16:06:23 +0700

    Finish 2

--- 08-Constructor-function/01/main.js
+++ 08-Constructor-function/01/main.js
@@ -1,16 +1,16 @@
 function calculator() {
-  (this.firstNum = 0),
-    (this.secondNum = 0),
-    (this.read = function () {
-      this.firstNum = +prompt('First number');
-      this.secondNum = +prompt('Second number');
-    }),
-    (this.sum = () => {
-      return this.firstNum + this.secondNum;
-    }),
-    (this.mul = () => {
-      return this.firstNum * this.secondNum;
-    });
+  this.firstNum = 0;
+  this.secondNum = 0;
+  this.read = function () {
+    this.firstNum = +prompt('First number');
+    this.secondNum = +prompt('Second number');
+  };
+  this.sum = () => {
+    return this.firstNum + this.secondNum;
+  };
+  this.mul = () => {
+    return this.firstNum * this.secondNum;
+  };
 }
 
 let obj = new calculator();
